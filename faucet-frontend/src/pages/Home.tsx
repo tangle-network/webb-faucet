f2da3beaf2b6d591f0fa020ef83ea7e25336006d
AtelyPham <tinpham1412000@gmail.com>
2023-03-31 00:50:08 +0700

    chore: fallback PUBLIC_URL to localhost

--- faucet-frontend/src/pages/Home.tsx
+++ faucet-frontend/src/pages/Home.tsx
@@ -1,73 +1,79 @@
-import React, { useEffect } from "react";
-import queryString from "query-string";
-import TwitterAuth from "../components/TwitterAuth";
-import axios from "axios";
-import ClaimFundsForm from "../components/ClaimFundsForm";
+import React, { useEffect } from 'react'
+import queryString from 'query-string'
+import TwitterAuth from '../components/TwitterAuth'
+import axios from 'axios'
+import ClaimFundsForm from '../components/ClaimFundsForm'
 
 const postData = async (code: string) => {
-  const client_id = process.env.REACT_APP_TWITTER_CLIENT_ID || "";
+  const client_id = process.env.REACT_APP_TWITTER_CLIENT_ID || ''
+  const publicUrl = process.env.PUBLIC_URL || 'http://localhost:3000'
+  console.log('publicUrl', publicUrl)
   const response = await axios.post(
     // This will be proxied to https:
     // "https://api.twitter.com/2/oauth2/token"
-    "/2/oauth2/token",
+    '/2/oauth2/token',
     {
       code,
-      grant_type: "authorization_code",
+      grant_type: 'authorization_code',
       client_id: client_id,
-      redirect_uri: process.env.PUBLIC_URL,
-      code_verifier: "challenge",
+      redirect_uri: publicUrl,
+      code_verifier: 'challenge',
     },
     {
-      baseURL: process.env.PUBLIC_URL,
+      baseURL: publicUrl,
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      }
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     }
-  );
-  return response.data;
-};
+  )
+  return response.data
+}
 
 const authenticate = async (accessToken: string) => {
-  const response = await axios.get(`/auth/twitter/?access_token=${accessToken}`, {
-    baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8000",
-  });
-  console.log(response);
-  return response.data;
-};
+  const response = await axios.get(
+    `/auth/twitter/?access_token=${accessToken}`,
+    {
+      baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000',
+    }
+  )
+  console.log(response)
+  return response.data
+}
 
 const Home: React.FC = () => {
-  const [accessToken, setAccessToken] = React.useState<string | null>(null);
-  const [refreshToken, setRefreshToken] = React.useState<string | null>(null);
-  const [scope, setScope] = React.useState<string | null>(null);
-  const query = queryString.parse(window.location.search);
-  const code = query.code;
-  const state = query.state;
+  const [accessToken, setAccessToken] = React.useState<string | null>(null)
+  const [refreshToken, setRefreshToken] = React.useState<string | null>(null)
+  const [scope, setScope] = React.useState<string | null>(null)
+  const query = queryString.parse(window.location.search)
+  const code = query.code
+  const state = query.state
 
   const onLogin = async () => {
-    const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
-    window.location.assign(`${backendUrl}/login/twitter`);
-  };
+    const backendUrl =
+      process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000'
+    window.location.assign(`${backendUrl}/login/twitter`)
+  }
 
   useEffect(() => {
     async function fetchData() {
       if (code && state) {
-        const data = await postData(code as string);
-        setAccessToken(data.access_token);
-        setRefreshToken(data.refresh_token);
-        setScope(data.scope);
-        const authenticateResponse = await authenticate(data.access_token);
-        console.log(authenticateResponse);
+        const data = await postData(code as string)
+        setAccessToken(data.access_token)
+        setRefreshToken(data.refresh_token)
+        setScope(data.scope)
+        const authenticateResponse = await authenticate(data.access_token)
+        console.log(authenticateResponse)
       }
     }
 
-    fetchData();
-  }, [code, state]);
+    fetchData()
+  }, [code, state])
 
   return (
-    <div style={{ margin: "32px", padding: "32px" }}>
+    <div style={{ margin: '32px', padding: '32px' }}>
       <TwitterAuth onLogin={onLogin} />
       {code && state ? (
-        <div style={{ marginTop: "32px" }}>
+        <div style={{ marginTop: '32px' }}>
           <p>
             <strong>Code:</strong> {code}
           </p>
@@ -77,7 +83,7 @@ const Home: React.FC = () => {
         </div>
       ) : null}
       {accessToken && refreshToken ? (
-        <div style={{ marginTop: "32px" }}>
+        <div style={{ marginTop: '32px' }}>
           <p>
             <strong>Access Token:</strong> {accessToken}
           </p>
@@ -86,9 +92,9 @@ const Home: React.FC = () => {
           </p>
         </div>
       ) : null}
-      <ClaimFundsForm accessToken={accessToken || ""} />
+      <ClaimFundsForm accessToken={accessToken || ''} />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home

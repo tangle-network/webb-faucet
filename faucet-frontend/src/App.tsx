2e758f682fd169647c540cefa18dc127d115a5ac
Shady Khalifa <dev+github@shadykhalifa.me>
2023-03-31 19:53:29 +0200

    Rework the simple frontend implementation with a new design

--- faucet-frontend/src/App.tsx
+++ faucet-frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Auth from './pages/Auth';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
 const App: React.FC = () => {
   return (
--- faucet-frontend/src/components/ClaimFundsForm.tsx
+++ faucet-frontend/src/components/ClaimFundsForm.tsx
@@ -1,86 +1,109 @@
-import axios, { AxiosResponse } from 'axios';
-import React, { useState } from 'react';
-import './ClaimFundsForm.css';
+import axios, { AxiosResponse } from "axios";
+import React, { useState } from "react";
+import "./ClaimFundsForm.css";
 
 interface ClaimFundsFormProps {
-  accessToken: string;
+  accessToken: string | null;
 }
 
-const ClaimFundsForm: React.FC<ClaimFundsFormProps> = ({accessToken }) => {
-  const [chain, setChain] = useState<number>(0);
-  const [address, setAddress] = useState<string>('');
+type EvmChain = { Evm: number };
+type SubstrateChain = { Substrate: number };
+type Chain = EvmChain | SubstrateChain;
+
+const ClaimFundsForm: React.FC<ClaimFundsFormProps> = ({ accessToken }) => {
+  const [chain, setChain] = useState<Chain>({ Evm: 5 });
+  const [address, setAddress] = useState<string>("");
 
   const claimFunds = async () => {
     // Implement your claim funds logic here
     let result;
     try {
       const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
-      result = await axios.post('/faucet',
+      result = await axios.post(
+        "/faucet",
         JSON.stringify({
           faucet: {
-            address: address,
-            typed_chain_id: String(chain),
+            walletAddress: {
+              type: (chain as EvmChain).Evm
+                ? "ethereum"
+                : (chain as SubstrateChain).Substrate
+                ? "substrate"
+                : "Unknown",
+              value: address,
+            },
+            typedChainId: {
+              type: (chain as EvmChain).Evm ? "Evm" : "Substrate",
+              id: (chain as EvmChain).Evm || (chain as SubstrateChain).Substrate,
+            },
           },
-          oauth: {
-            access_token: accessToken,
-          }
-        }), {
+        }),
+        {
           headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Access-Control-Allow-Origin': '*',
-            'Authorization': 'Bearer ' + accessToken,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin": "*",
+            Authorization: "Bearer " + accessToken,
           },
           baseURL: backendUrl,
         }
       );
     } catch (error) {
       console.error("Error response:");
-      console.error((error as any).response.data);    // ***
-      console.error((error as any).response.status);  // ***
+      console.error((error as any).response.data); // ***
+      console.error((error as any).response.status); // ***
       console.error((error as any).response.headers); // ***
+      alert("Error claiming funds: check the console for more details");
+      return;
     }
 
     // Parse the response and display the result to the user
-    console.log('Funds claimed successfully');
+    console.log("Funds claimed successfully");
     console.log((result as AxiosResponse).data);
 
-    // Reset the form
-    setChain(0);
-    setAddress('');
+    alert("Funds claimed successfully");
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    console.log('Submitting form');
+    if (accessToken === null) {
+      console.log("Not logged in");
+      return;
+    }
+    console.log("Submitting form");
     event.preventDefault();
     console.log(`Claiming funds for chain: ${chain}, address: ${address}`);
     claimFunds();
   };
 
   return (
-    <div className="formWrapper">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="chain">Chain:</label>
-        <select
-          id="chain"
-          value={chain}
-          onChange={(event) => setChain(Number(event.target.value))}
-        >
-          <option value={0}>Chain 0</option>
-          <option value={1}>Chain 1</option>
-          <option value={2}>Chain 2</option>
-        </select>
+    <form className="claim-funds-form" onSubmit={handleSubmit}>
+      <label htmlFor="chain">Chain:</label>
+      <select
+        className="chain-selector"
+        id="chain"
+        multiple={false}
+        value={JSON.stringify(chain)}
+        onChange={(event) => setChain(JSON.parse(event.target.value) as Chain)}
+      >
+        <option value={JSON.stringify({ Evm: 5 })}>Gorli</option>
+        <option value={JSON.stringify({ Evm: 80001 })}>Mumbai</option>
+        <option value={JSON.stringify({ Substrate: 1080 })}>Tangle Standalone</option>
+      </select>
 
-        <label htmlFor="address">Address:</label>
-        <input
-          type="text"
-          id="address"
-          value={address}
-          onChange={(event) => setAddress(event.target.value)}
-        />
+      <label htmlFor="address">Address:</label>
+      <input
+        type="text"
+        id="address"
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
+      />
 
-        <button type="submit">Claim</button>
-      </form>
-    </div>
+      <button
+        className="submit-button"
+        disabled={accessToken === null ? true : false}
+        type="submit"
+      >
+        Claim
+      </button>
+    </form>
   );
 };
 

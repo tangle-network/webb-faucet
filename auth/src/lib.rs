702480a74c0075077b5ab884accf1990c3a231ef
Shady Khalifa <dev+github@shadykhalifa.me>
2023-03-31 19:49:06 +0200

    remove authorizer

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -1,53 +1,13 @@
-use model::Authorization;
-use std::marker::PhantomData;
-
 pub mod db;
 pub mod model;
 
 pub use db::AuthDb;
-pub use model::{Provider, UserInfo};
+pub use model::UserInfo;
 
-pub struct Authorizer<A: AuthDb> {
-    _auth_db: PhantomData<A>,
-}
-
-impl<A: AuthDb> Authorizer<A> {
-    pub async fn open() -> Result<Self, Error<A::Error>> {
-        Ok(Self {
-            _auth_db: PhantomData,
-        })
-    }
-
-    pub async fn authorize_twitter(
-        &self,
-        connection: &A::Connection,
-        token: &str,
-    ) -> Result<Option<Authorization>, Error<A::Error>> {
-        if let Some(id) = A::lookup_twitter_token(connection, token)
-            .await
-            .map_err(Error::AuthDb)?
-        {
-            A::save_authorization(connection, id)
-                .await
-                .map_err(Error::AuthDb)
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub async fn get_user_info(
-        &self,
-        connection: &A::Connection,
-        id: &u64,
-    ) -> Result<Option<UserInfo>, Error<A::Error>> {
-        Ok(A::get_twitter_name(connection, *id)
-            .await
-            .map_err(Error::AuthDb)?)
-    }
-}
-
-#[derive(thiserror::Error, Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error<E: std::error::Error> {
-    #[error("Auth DB error")]
+    #[error("AuthDb error: {0}")]
     AuthDb(E),
+    #[error("Custom OAuth 2.0 error: {0}")]
+    Custom(String),
 }

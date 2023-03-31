9f43aaa4d08def030a3e5f88ff5f58d593cabb5e
Shady Khalifa <dev+github@shadykhalifa.me>
2023-03-31 19:49:32 +0200

    simplify the authdb interface

--- auth/src/db.rs
+++ auth/src/db.rs
@@ -1,69 +1,26 @@
 use chrono::{DateTime, Utc};
-use egg_mode::Token;
 use webb_proposals::TypedChainId;
 
-use crate::{
-    model::{Authorization, ClaimsData},
-    UserInfo,
-};
+use crate::{model::ClaimsData, UserInfo};
 
 #[async_trait::async_trait]
 pub trait AuthDb {
-    type Connection;
     type Error: std::error::Error;
 
-    async fn get_twitter_name(
-        connection: &Self::Connection,
-        id: u64,
-    ) -> Result<Option<UserInfo>, Self::Error>;
+    async fn get_user_info(&self, id: u64) -> Result<Option<UserInfo>, Self::Error>;
 
-    async fn put_twitter_name(
-        connection: &Self::Connection,
-        id: u64,
-        value: &str,
-    ) -> Result<(), Self::Error>;
+    async fn put_user_info(&self, id: u64, value: &UserInfo) -> Result<(), Self::Error>;
 
-    async fn put_twitter_name_with_address(
-        connection: &Self::Connection,
-        id: u64,
-        value: &str,
-        address: Vec<u8>,
-    ) -> Result<(), Self::Error>;
-
-    async fn lookup_twitter_token(
-        connection: &Self::Connection,
-        token: &str,
-    ) -> Result<Option<u64>, Self::Error>;
-
-    async fn get_twitter_access_token(
-        connection: &Self::Connection,
-        token: &str,
-    ) -> Result<Option<Token>, Self::Error>;
-
-    async fn put_twitter_token(
-        connection: &Self::Connection,
-        token: &str,
-        id: u64,
-        consumer_secret: &str,
-        access_key: &str,
-        access_secret: &str,
-    ) -> Result<(), Self::Error>;
-
-    async fn save_authorization(
-        connection: &Self::Connection,
-        id: u64,
-    ) -> Result<Option<Authorization>, Self::Error>;
-
-    async fn put_last_claim_date(
-        connection: &Self::Connection,
+    async fn put_last_claim_data(
+        &self,
         id: u64,
         typed_chain_id: TypedChainId,
         claim: ClaimsData,
     ) -> Result<DateTime<Utc>, Self::Error>;
 
-    async fn get_last_claim_date(
-        connection: &Self::Connection,
+    async fn get_last_claim_data(
+        &self,
         id: u64,
         typed_chain_id: TypedChainId,
-    ) -> Result<Option<DateTime<Utc>>, Self::Error>;
+    ) -> Result<Option<ClaimsData>, Self::Error>;
 }

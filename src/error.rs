c372b08df4595bfd3c63ae582071d578c5b42cfe
Shady Khalifa <dev+github@shadykhalifa.me>
2023-03-31 19:51:29 +0200

    rework the error handling

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,4 @@
 use rocket::{
-    http::Status,
     request::Request,
     response::{Responder, Result},
     serde::json::Json,
@@ -7,53 +6,106 @@ use rocket::{
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("OAuth 2.0 error")]
+    #[error("OAuth 2.0 error: {0}")]
     Oauth2(#[from] rocket_oauth2::Error),
-    #[error("Authorization error")]
-    Authorization(#[from] webb_auth::Error<webb_auth_sled::Error>),
-    #[error("Twitter error")]
+    #[error("Database error: {0}")]
+    AuthDatabase(#[from] webb_auth_sled::Error),
+    #[error("Twitter error: {0}")]
     TwitterError(#[from] twitter_v2::error::Error),
-    #[error("Custom error")]
+    #[error("Custom error: {0}")]
     Custom(String),
+}
+
+#[derive(Copy, Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub enum FaucetErrorCode {
+    /// An error occurred during URI parsing or construction. This usually means
+    /// the token exchange endpoint is incorrect. The attempted URI is included.
+    Oauth2InvalidUri = 100000,
+    /// A token exchange request failed, for example because the server could
+    /// not be reached, or the response body could not be parsed.
+    Oauth2ExchangeFailure = 100001,
+    /// An unknown error occurred during token exchange.
+    Oauth2Unknown = 100002,
+    /// A Database error occurred.
+    DatabaseError = 200000,
+    /// A Data model serialization error occurred.
+    DataSerializationError = 200001,
+    /// An error occurred while communicating with Twitter API.
+    TwitterApiError = 300000,
+    /// An Unknown error occurred.
+    CustomError = 400000,
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct ErrorResponse {
-    pub status: &'static str,
+    pub code: FaucetErrorCode,
     pub message: String,
 }
 
 impl<'r, 'o: 'r> Responder<'r, 'o> for Error {
     fn respond_to(self, req: &'r Request<'_>) -> Result<'o> {
         match self {
-            Error::Oauth2(e) => Json(ErrorResponse {
-                status: "oauth2 error",
-                message: e.to_string(),
-            })
-            .respond_to(req),
-            Error::Authorization(_) => Status::InternalServerError.respond_to(req),
-            Error::TwitterError(e) => match e {
-                twitter_v2::Error::Url(e) => Json(ErrorResponse {
-                    status: "url error",
-                    message: e.to_string(),
-                })
-                .respond_to(req),
-                twitter_v2::Error::InvalidAuthorizationHeader(_) => {
-                    Status::Unauthorized.respond_to(req)
+            Error::Oauth2(ref err) => match err.kind() {
+                rocket_oauth2::ErrorKind::InvalidUri(_) => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::Oauth2InvalidUri,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
                 }
-                twitter_v2::Error::NoRefreshToken => Status::NotFound.respond_to(req),
-                twitter_v2::Error::Custom(e) => Json(ErrorResponse {
-                    status: "custom error",
-                    message: e,
-                })
-                .respond_to(req),
-                _ => Status::InternalServerError.respond_to(req),
+                rocket_oauth2::ErrorKind::ExchangeFailure
+                | rocket_oauth2::ErrorKind::ExchangeError(_) => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::Oauth2ExchangeFailure,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
+                }
+                rocket_oauth2::ErrorKind::Other => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::Oauth2Unknown,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
+                }
             },
-            Error::Custom(_) => Json(ErrorResponse {
-                status: "custom error",
-                message: self.to_string(),
-            })
-            .respond_to(req),
+            Error::AuthDatabase(ref err) => match err {
+                webb_auth_sled::Error::Sled(_) => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::DatabaseError,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
+                }
+                webb_auth_sled::Error::InvalidId(_) => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::DataSerializationError,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
+                }
+                webb_auth_sled::Error::Serde(_) => {
+                    let response = ErrorResponse {
+                        code: FaucetErrorCode::DataSerializationError,
+                        message: self.to_string(),
+                    };
+                    Json(response).respond_to(req)
+                }
+            },
+            Error::TwitterError(_) => {
+                let response = ErrorResponse {
+                    code: FaucetErrorCode::TwitterApiError,
+                    message: self.to_string(),
+                };
+                Json(response).respond_to(req)
+            }
+            Error::Custom(_) => {
+                let response = ErrorResponse {
+                    code: FaucetErrorCode::CustomError,
+                    message: self.to_string(),
+                };
+                Json(response).respond_to(req)
+            }
         }
     }
 }

3e18042d0c4904c3eb86770be9f7e820205348ba
Shady Khalifa <dev+github@shadykhalifa.me>
2023-06-02 14:47:38 +0300

    Make it runs

--- src/txes/types.rs
+++ src/txes/types.rs
@@ -1,15 +1,30 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
 use ethers::types::U256;
 use ethers::{
+    prelude::*,
     providers::{Http, Provider},
     types::{Address, TransactionReceipt},
 };
 use rocket::tokio::sync::oneshot;
 use serde::{Deserialize, Serialize};
+use webb::evm::ethers;
 use webb::substrate::subxt::{utils::AccountId32, OnlineClient, PolkadotConfig};
 
 use crate::error::Error;
+
+pub type EthersClient = Arc<
+    NonceManagerMiddleware<
+        SignerMiddleware<
+            gas_oracle::GasOracleMiddleware<
+                gas_escalator::GasEscalatorMiddleware<Provider<Http>>,
+                gas_oracle::GasNow,
+            >,
+            LocalWallet,
+        >,
+    >,
+>;
 
 pub struct EvmProviders<T> {
     pub providers: HashMap<u64, T>,
@@ -25,9 +40,10 @@ pub enum TxResult {
     Substrate(webb::substrate::subxt::utils::H256),
 }
 
+#[allow(clippy::large_enum_variant)]
 pub enum Transaction {
     Evm {
-        provider: Provider<Http>,
+        provider: EthersClient,
         to: Address,
         amount: U256,
         token_address: Option<Address>,
@@ -41,4 +57,41 @@ pub enum Transaction {
         signer: sp_core::sr25519::Pair,
         result_sender: oneshot::Sender<Result<TxResult, Error>>,
     },
+}
+
+impl std::fmt::Debug for Transaction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Evm {
+                provider,
+                to,
+                amount,
+                token_address,
+                result_sender,
+            } => f
+                .debug_struct("Evm")
+                .field("provider", provider)
+                .field("to", to)
+                .field("amount", amount)
+                .field("token_address", token_address)
+                .field("result_sender", result_sender)
+                .finish(),
+            Self::Substrate {
+                api,
+                to,
+                amount,
+                asset_id,
+                result_sender,
+                ..
+            } => f
+                .debug_struct("Substrate")
+                .field("api", api)
+                .field("to", to)
+                .field("amount", amount)
+                .field("asset_id", asset_id)
+                .field("signer", &"<hidden>")
+                .field("result_sender", result_sender)
+                .finish(),
+        }
+    }
 }

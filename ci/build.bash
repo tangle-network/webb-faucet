52675fff57ebfb599aed889915bf164990acd6f9
Shady Khalifa <dev+github@shadykhalifa.me>
2023-06-07 19:33:10 +0300

    add CI scripts

---
+++ ci/build.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Script for building your rust projects.
+set -e
+
+source ci/common.bash
+
+# $1 {path} = Path to cross/cargo executable
+CROSS=$1
+# $2 {string} = <Target Triple> e.g. x86_64-pc-windows-msvc
+TARGET_TRIPLE=$2
+# $3 {boolean} = Are we building for deployment?
+RELEASE_BUILD=$3
+# $4 {boolean} = Are we building for docker?
+RELEASE_DOCKER=$4
+
+required_arg $CROSS 'CROSS'
+required_arg $TARGET_TRIPLE '<Target Triple>'
+
+if [ -z "$RELEASE_BUILD" ]; then
+    $CROSS build --target $TARGET_TRIPLE
+    $CROSS build --target $TARGET_TRIPLE --all-features
+else
+    $CROSS build --target $TARGET_TRIPLE --all-features --release
+fi
+
+if [ -z "$RELEASE_DOCKER" ]; then
+    # Do nothing.
+    true
+else
+    # build for docker
+    $CROSS build -p webb-faucet --target "$TARGET_TRIPLE" --release
+    mkdir -p build && cp target/"$TARGET_TRIPLE"/release/webb-faucet build/webb-faucet
+fi
